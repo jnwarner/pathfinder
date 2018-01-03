3f05b63625363b323a565aa3dc160bb3302e8250
jnwarner <joshnwarner56@gmail.com>
2018-01-02 19:35:50 -0600

    Added initial files for pathfinder class

---
+++ src/main/pathfinder.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Joshua Warner on 1/1/18.
+//
+
+#include "pathfinder.h"
---
+++ src/main/pathfinder.h
@@ -0,0 +1,17 @@
+//
+// Created by Joshua Warner on 1/1/18.
+//
+
+#ifndef PATHFINDER_PATHFINDER_H
+#define PATHFINDER_PATHFINDER_H
+
+
+class pathfinder {
+private:
+    // TODO: Consider needs
+public:
+    // TODO: Consider needs
+};
+
+
+#endif //PATHFINDER_PATHFINDER_H
